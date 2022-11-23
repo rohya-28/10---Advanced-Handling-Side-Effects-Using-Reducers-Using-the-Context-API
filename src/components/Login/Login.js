e70869c2e23ea1460b5839eb04b8d9c3dbae49b2
rohya-28 <rohitarsude28@gmail.com>
2022-11-22 23:32:27 -0400

    cleanUp Function in useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('Its Triggered');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      )
+    },1000)
+   
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier)
+    }
+  
   }, [enteredEmail,enteredPassword])
 
   const emailChangeHandler = (event) => {
